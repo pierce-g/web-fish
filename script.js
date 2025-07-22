2ed023c8e10f50dfd05ee64125171b4792784d32
pierce-g <piercejgorman@gmail.com>
2025-07-22 18:03:06 +0100

    Probably should've staged these wayyy earlier. Added canvas with random moving fish.

---
+++ script.js
@@ -0,0 +1,94 @@
+var c = document.getElementById("fishtank");
+var ctx = c.getContext("2d");
+var fish = document.getElementById('fishImg');
+var addFishButton = document.getElementById("add-fish");
+
+addFishButton.addEventListener("click", addFish);
+
+var fishes = [];
+
+var interval = setInterval(draw, 50);
+
+
+class Fish {
+    constructor(img, posX, posY, width, height){
+        this.img = img;
+        this.posX = posX;
+        this.posY = posY;
+        this.width = width;
+        this.height = height;
+        this.state = "IDLE"
+        
+        this.targetPos = [0,0];
+        this.reachedTarget = true;
+
+        this.states = ["SWIM", "IDLE", "FEEDING"]
+    }
+
+    swim(swimX, swimY){
+        this.posX += swimX;
+        this.posY += swimY;
+
+        //Check collision with edge
+        
+        this.posX = (this.posX < this.width) ? this.width : this.posX;
+        this.posX = (this.posX > c.width-this.width) ? c.width-this.width : this.posX;
+
+        this.posY = (this.posY < this.height) ? this.height : this.posY;
+        this.posY = (this.posY > c.height-this.height) ? c.height-this.height : this.posY;
+    }
+
+    doThing(){
+        switch(this.state){
+            case "IDLE":
+                this.idle();
+            case "SWIM":
+                this.swim();
+
+        }
+    }
+
+    idle(){
+        this.state = this.randomSwitchState("SWIM");
+    }
+
+    swim(){
+        if(this.posX == this.targetPos[0] && this.posY == this.targetPos[1]){
+            this.reachedTarget = true;
+        }
+
+        if(this.reachedTarget){
+            this.targetPos = [getRandomInt(c.width), getRandomInt(c.height)];
+            this.reachedTarget = false;
+        }
+        if(this.posX != this.targetPos[0]){
+            this.posX = (this.posX < this.targetPos[0]) ? this.posX + 1 : this.posX -1;
+        }
+        if(this.posY != this.targetPos[1]){
+            this.posY = (this.posY < this.targetPos[1]) ? this.posY + 1 : this.posY -1;
+        }
+
+        this.state = this.randomSwitchState("IDLE");
+    }
+
+    randomSwitchState(new_state){
+        return (getRandomInt(100) == 0) ? new_state : this.state;
+    }
+}
+
+function addFish(){
+    console.log("add fish :)")
+    fishes.push(new Fish(fish, 0, 0, 30, 30));
+}
+
+function draw(){
+    ctx.clearRect(0,0,c.width, c.height);
+    
+    fishes.forEach(element => {
+        element.doThing();
+        ctx.drawImage(element.img, element.posX, element.posY, element.width, element.height);
+    });
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
