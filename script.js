fcc7630aeff7d31affae65be9a7dc599b74a0e40
pierce-g <piercejgorman@gmail.com>
2025-07-22 20:36:30 +0100

    fixed fish not being able to go to the left and top of tank. you can name them aswell now :)

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var ctx = c.getContext("2d");
 var fish = document.getElementById('fishImg');
 var fishLeft = document.getElementById('fishLeft');
 var moneyImg = document.getElementById('money')
+var fishName = document.getElementById('nameInput')
 
 var moneyText = document.getElementById("moneyText");
 
@@ -58,13 +59,14 @@ class Money{
 
 
 class Fish {
-    constructor(img, posX, posY, width, height){
+    constructor(img, posX, posY, width, height, name){
         this.img = img;
         this.posX = posX;
         this.posY = posY;
         this.width = width;
         this.height = height;
         this.state = "IDLE"
+        this.name = name;
         
         this.targetPos = [0,0];
         this.reachedTarget = true;
@@ -112,12 +114,10 @@ class Fish {
         if(this.reachedTarget){
             this.targetPos = [getRandomInt(c.width), getRandomInt(c.height)];
 
-            //Width and height check
-            this.targetPos[0] = (this.targetPos[0] < this.width) ? this.width : this.targetPos[0];
-            this.targetPos[0] = (this.targetPos[0] > c.width - this.width) ? c.width-this.width : this.targetPos[0];
+            // Clamp target position to keep the object fully within the canvas
+            this.targetPos[0] = Math.max(0, Math.min(this.targetPos[0], c.width - this.width));
+            this.targetPos[1] = Math.max(0, Math.min(this.targetPos[1], c.height - this.height));
 
-            this.targetPos[1] = (this.targetPos[1] < this.height) ? this.height : this.targetPos[1];
-            this.targetPos[1] = (this.targetPos[1] > c.height - this.height) ? c.height-this.height : this.targetPos[1];
 
 
             this.reachedTarget = false;
@@ -153,7 +153,7 @@ class Fish {
 function addFish(){
     console.log("add fish :) cost: 100")
     if(money >= 100){
-        fishes.push(new Fish(fish, 0, 0, (c.width / 5), (c.height /5)));
+        fishes.push(new Fish(fish, 0, 0, (c.width / 5), (c.height /5), fishName.value));
         money -= 100;
         moneys.push(new Money(moneyImg, 460, 400, 50));
     }
@@ -166,6 +166,7 @@ function draw(){
     fishes.forEach(element => {
         element.doThing();
         ctx.drawImage(element.img, element.posX, element.posY, element.width, element.height);
+        ctx.fillText(element.name, element.posX+(element.width/2), element.posY);
     });
 
     moneys.forEach(element => {
